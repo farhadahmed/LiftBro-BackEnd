89a515e44d4190de057c7abf356862dfe3e20fda
Farhad Ahmed <farhadahmed2007@gmail.com>
2023-07-17 12:09:22 -0700

    add home page

--- src/main/java/com/liftbro/backend/controllers/WebController.java
+++ src/main/java/com/liftbro/backend/controllers/WebController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
     @GetMapping("/")
-    public String getHomepage() {
+    public String getHomePage() {
         return "<h1> Lift Bro home page placeholder </h1>";
-
     }
 }
