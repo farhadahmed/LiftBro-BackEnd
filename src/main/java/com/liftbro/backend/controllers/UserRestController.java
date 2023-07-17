e5eedd47bdd8d28747a07065ddba2463eb8e6c48
Farhad Ahmed <farhadahmed2007@gmail.com>
2023-07-17 16:43:02 -0700

    convert UsersController to use the route "users" instead of user2

--- src/main/java/com/liftbro/backend/controllers/UserRestController.java
+++ src/main/java/com/liftbro/backend/controllers/UserRestController.java
@@ -1,47 +1,47 @@
-package com.liftbro.backend.controllers;
-
-import com.liftbro.backend.models.UsersTest;
-import com.liftbro.backend.services.UsersServiceTODELETE;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-public class UserRestController {
-
-    @Autowired
-    private UsersServiceTODELETE userService;
-
-    @PostMapping("/users")
-    public ResponseEntity<String> createUser(@RequestBody UsersTest user) {
-        String status = userService.upsert(user);
-        return new ResponseEntity<>(status, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/users/{users_id}")
-    public ResponseEntity<UsersTest> getUser(@PathVariable Integer users_id) {
-        UsersTest user = userService.getById(users_id);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<List<UsersTest>> getAllUsers() {
-        List<UsersTest> allUsers = userService.getAllUsers();
-        return new ResponseEntity<>(allUsers, HttpStatus.OK);
-    }
-
-    @PutMapping("/users")
-    public ResponseEntity<String> updateUser(@RequestBody UsersTest user) {
-        String status = userService.upsert(user);
-        return new ResponseEntity<>(status, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/users/{users_id}")
-    public ResponseEntity<String> deleteUser(@PathVariable Integer users_id) {
-        String status = userService.deleteById(users_id);
-        return new ResponseEntity<>(status, HttpStatus.OK);
-    }
-}
+//package com.liftbro.backend.controllers;
+//
+//import com.liftbro.backend.models.UsersTest;
+//import com.liftbro.backend.services.UsersServiceTODELETE;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//public class UserRestController {
+//
+//    @Autowired
+//    private UsersServiceTODELETE userService;
+//
+//    @PostMapping("/users")
+//    public ResponseEntity<String> createUser(@RequestBody UsersTest user) {
+//        String status = userService.upsert(user);
+//        return new ResponseEntity<>(status, HttpStatus.CREATED);
+//    }
+//
+//    @GetMapping("/users/{users_id}")
+//    public ResponseEntity<UsersTest> getUser(@PathVariable Integer users_id) {
+//        UsersTest user = userService.getById(users_id);
+//        return new ResponseEntity<>(user, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/users")
+//    public ResponseEntity<List<UsersTest>> getAllUsers() {
+//        List<UsersTest> allUsers = userService.getAllUsers();
+//        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+//    }
+//
+//    @PutMapping("/users")
+//    public ResponseEntity<String> updateUser(@RequestBody UsersTest user) {
+//        String status = userService.upsert(user);
+//        return new ResponseEntity<>(status, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/users/{users_id}")
+//    public ResponseEntity<String> deleteUser(@PathVariable Integer users_id) {
+//        String status = userService.deleteById(users_id);
+//        return new ResponseEntity<>(status, HttpStatus.OK);
+//    }
+//}
