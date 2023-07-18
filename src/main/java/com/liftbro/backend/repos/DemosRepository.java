c4e19520e0a921bc7c570542189451c6f447d7b9
Farhad Ahmed <farhadahmed2007@gmail.com>
2023-07-17 19:51:09 -0700

    removed all Demo-related classes that were created for testing

--- src/main/java/com/liftbro/backend/repos/DemosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.liftbro.backend.repos;
-
-import com.liftbro.backend.models.Demos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.io.Serializable;
-
-public interface DemosRepository extends JpaRepository<Demos, Serializable> {
-}
